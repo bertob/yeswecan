9dcf27b464e1b0125dc121851ca4d5bd7e2f321b
bertob <bertob@zenbook>
2015-07-21 15:53:02 +0200

    [feat] add links to caniuse tooltip descriptions, [ui] add browser icons

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $.getJSON("features.json", function(data){
-  renderData(data);
+  $.getJSON("meta.json", function(meta){
+    renderData(data, meta);
+  });
 });
 
-function renderData(data) {
+function renderData(data, meta) {
   var i = 0;
   for (var feature in data) {
     if(i === 0 || (i % 25) === 0) {
@@ -10,17 +12,21 @@ function renderData(data) {
       block.append(
         "<div class='row browser-labels'>" +
           "<div class='left-col'></div>" +
-          "<label>IE</label>" +
-          "<label>ED</label>" +
-          "<label>FX</label>" +
-          "<label>CH</label>" +
-          "<label>SF</label>" +
-          "<label>OP</label>" +
+          "<img src='img/ie.png' title='Internet Explorer'>" +
+          "<img src='img/edge.png' title='Microsoft Edge'>" +
+          "<img src='img/firefox.png' title='Firefox'>" +
+          "<img src='img/chrome.png' title='Google Chrome'>" +
+          "<img src='img/safari.png' title='Safari'>" +
+          "<img src='img/opera.png' title='Opera'>" +
         "</div>");
       i = 0;
     }
     var row = $("<div class='row'></div>");
-    row.append("<label class='left-col'>" + feature + "</label>");
+    row.append("<label class='left-col' title='" +
+        meta[feature].title + ": " + meta[feature].description + "'>" +
+        "<a href='http://caniuse.com/#search=" + feature +"'>" +
+        meta[feature].title + "</a>" +
+      "</label>");
     var arr = data[feature];
     for(var j=0; j<arr.length; j++) {
       row.append("<div class='point " + arr[j] + "' title='" + "'></div>");
